5f06b50d2772f44289e71ca4d75f36c1f46bac36
hoangkhai2004 <nguyenhoangkhai2029@gmail.com>
2024-07-31 22:32:01 +0700

    Xây dựng UI phần sidebar #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import Header from '~/layouts/components/Header';
+import Sidebar from '~/layouts/components/SideBar';
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
-import Sidebar from './SideBar';
 
 const cx = classNames.bind(styles);
 function DelaultLayout({ children }) {
---
+++ src/pages/Live/Live.js
@@ -0,0 +1,5 @@
+function Live() {
+    return <h1>LIVE PAGE</h1>;
+}
+
+export default Live;
